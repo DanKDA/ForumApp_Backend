4127eb16686d760df710e5227c92bac5cccf2818
GheorgheKDA <mihalachigheorghe05@gmail.com>
2026-03-25 19:43:08 +0200

    Merge branch 'gheorghe' of https://github.com/DanKDA/ForumApp_Backend

--- ForumApp.Domain/Entities/Contact/Contact.cs
+++ ForumApp.Domain/Entities/Contact/Contact.cs
@@ -35,7 +35,7 @@ namespace ForumApp.Domain.Entities.Contact
         [Required]
         [StringLength(1000)]
         public string Message {get; set;} = string.Empty;
-        public DateTime CreatedAt {get; set;} /* = DateTime.UtcNow; */
+        public DateTime CreatedAt {get; set;} = DateTime.UtcNow; 
 
     }
 }
