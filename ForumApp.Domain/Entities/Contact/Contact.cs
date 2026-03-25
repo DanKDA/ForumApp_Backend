a074164035daf56a2c2d4b27febbec64be746621
GheorgheKDA <mihalachigheorghe05@gmail.com>
2026-03-25 16:32:53 +0200

    fix problema push contact

---
+++ ForumApp.Domain/Entities/Contact/Contact.cs
@@ -0,0 +1,23 @@
+namespace ForumApp.Domain.Entities.Contact
+{
+    public enum ContactType
+    {
+        GeneralInquiry = 0,
+        AccountAndAuthentication = 1,
+        CommunitiesAndModeration = 2,
+        ReportAnIssue = 3,
+        LegalQuestions = 4
+    }
+    public class contactData
+    {
+        public int Id {get ; set;}
+        public string FullName {get; set;} = string.Empty;
+        public string Email {get; set;} = string.Empty;
+        public string Subject {get; set;} = string.Empty;
+        public ContactType Type {get; set;}
+        public string Message {get; set;} = string.Empty;
+        public DateTime CreatedAt {get; set;} /* = DateTime.UtcNow; */
+        public string ipAddress {get; set;} = string.Empty;
+
+    }
+}
