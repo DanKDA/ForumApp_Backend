86174d4baab327658eaf73c2cf42f2be532fad82
DanKDA <danbotan62@gmail.com>
2026-05-15 01:56:46 +0300

    Adaugarea de campuri in DTO pentru imagini, implementarea si permiterea in serviciu si Controler ca imaginile sa fie stocate pe server si cu referinta ca link in baza de date

--- ForumApp.Domain/Models/User/UserUpdateDto.cs
+++ ForumApp.Domain/Models/User/UserUpdateDto.cs
@@ -4,6 +4,7 @@ namespace ForumApp.Domain.Models.User
     {
         public string? UserName { get; set; }
         public string? Bio { get; set; }
+        public string? AvatarUrl { get; set; }
         public string? Theme { get; set; }
         public string? Language { get; set; }
         public string? ProfileVisibility { get; set; }
